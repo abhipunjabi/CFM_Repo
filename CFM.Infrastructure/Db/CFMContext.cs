caaff51c87e73507639660a0bf7cf6a2ea385f36
Abhishek Punjabi <Abhishek.Punjabi@civica2.com>
2020-11-12 15:53:25 +0530

    Add project files.

---
+++ CFM.Infrastructure/Db/CFMContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace CFM.Infrastructure.Db
+{
+    public class CFMContext : DbContext
+    {
+        private readonly IConfiguration _config;
+        public CFMContext(
+            DbContextOptions options,
+            IConfiguration config)
+            : base(options)
+        {
+            this._config = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("CFM"));
+        }
+    }
+}
